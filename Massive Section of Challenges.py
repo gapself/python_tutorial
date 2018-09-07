80f4f32ae6b1cc71d7e2ebc3125f5ff57f3c6da5
gapself <gabriela.paszkiewicz@gmail.com>
2018-09-07 19:02:50 +0200

    Remove_every_other

--- Massive Section of Challenges.py
+++ Massive Section of Challenges.py
@@ -11,3 +11,14 @@ print(list_check([[],[1],[2,3], (1,2)])) #False
 print(list_check([1, True, [],[1],[2,3]])) # False
 print(list_check([[],[1],[2,3]])) # True
 
+# remove_every_other
+def remove_every_other(list):
+    if len(list)>1:
+        return list[0::2]
+    else:
+        return list
+
+print(remove_every_other([1,2,3,4,5])) # [1,3,5]
+print(remove_every_other([5,1,2,4,1])) # [5,2,1]
+print(remove_every_other([1])) # [1]
+print(remove_every_other([1,2,3,4,5,8,9,0])) # [1,3,5]
