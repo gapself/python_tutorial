7ec0f22f96d3bdbf5fc52c23eadc7aa379bde696
gapself <gabriela.paszkiewicz@gmail.com>
2018-09-08 17:26:45 +0200

    Vowel count exe

--- Massive Section of Challenges.py
+++ Massive Section of Challenges.py
@@ -26,18 +26,21 @@ print(remove_every_other([1,2,3,4,5,8,9,0])) # [1,3,5,9]
 # ar[i]+ar[j]//k
 # ar[0]+ar[1]==6
 
-# my solution!!
+# my solution!! #błąd, np ([4,5,6],8) =>używa [4,4] tych samych cyfr ..
 def sum_pairs(a,num):
     for i in a: #(for loop dla każdego x)"
-        li= a[:i:]
-        for s in li:
+        for s in a[:i:]:
             if i+s == num:
                 return [i,s]
-        return []
-print(sum_pairs([4, 2, 10, 5, 1], 6))  # [4,2]
-print(sum_pairs([11, 20, 4, 2, 1, 5], 100))  # []
+    return []
 
-# my similar solution from hackerrank.com
+print(sum_pairs([4, 2, 10, 5, 1], 6))  # [4,2]
+print(sum_pairs([4, 2, 10, 5, 1], 8))  # []
+print(sum_pairs([11, 20, 4, 2, 1, 5], 100))  # []
+print(sum_pairs([3, 4, 5, 2, 3, 1, 7, 3, 4, 5], 9))
+print(sum_pairs([3, 4, 5, 2, 3, 1, 7, 3, 4, 5, 10, 20, 30, 12], 12))
+
+# # my similar solution from hackerrank.com
 def sum_pairs(a,num):
     for i in range(len(a) - 1):
         # print(i) 0,1,2,3,
@@ -45,7 +48,24 @@ def sum_pairs(a,num):
             # print(x) i=0#x=1234 1#234 2#34 3#4
             if (a[i] + a[x]) == num:
                 return [a[i], a[x]]
-            return []
+    return []
+print(sum_pairs([4, 2, 10, 5, 1], 8))  # [4,2]
 
-print(sum_pairs([4, 2, 10, 5, 1], 6))  # [4,2]
-print(sum_pairs([11, 20, 4, 2, 1, 5], 100))  # []
+# solution
+def sum_pairs(ints, s):
+     already_visited = set()
+     for i in ints:
+         difference = s - i
+         if difference in already_visited:
+             return [difference, i]
+         already_visited.add(i)
+     return []
+
+# VOWEL_COUNT
+def vowel_count(string):
+    return {x:string.lower().count(x) for x in string if x in "aeiou"}
+
+print(vowel_count('awesome')) # {'a': 1, 'e': 2, 'o': 1})
+print(vowel_count('Elie'))   # {'e': 2, 'i': 1}
+print(vowel_count('Colt'))  # {'o': 1}
+
