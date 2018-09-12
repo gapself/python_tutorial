be80bc66815a3f24c8399fc32dc8e864835e9c38
gapself <gabriela.paszkiewicz@gmail.com>
2018-09-12 23:37:23 +0100

    Migratory Birds

--- hackerrank/algorithms.py
+++ hackerrank/algorithms.py
@@ -19,3 +19,10 @@ for i in range(len(arr) - 1):
             count += 1
 
 print(count)
+
+# Migratory Birds
+input()
+count = [0]*6
+for t in map(int,input().strip().split()):
+    count[t] += 1
+print(count.index(max(count)))
