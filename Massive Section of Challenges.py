fa445017cafd2c846a6adf972db9d6c74ed592b5
gapself <gabriela.paszkiewicz@gmail.com>
2018-09-29 23:42:45 -1200

    NTH

--- Massive Section of Challenges.py
+++ Massive Section of Challenges.py
@@ -168,3 +168,16 @@ def two_list_dictionary(keys, values):
 
     return collection
 
+#NTH
+def nth(arr, idx):
+    if idx >= 0:
+        return arr[idx]
+    return arr[idx + len(arr)]
+
+
+print(nth(['a', 'b', 'c', 'd'], 1))  # 'b'
+print(nth(['a', 'b', 'c', 'd'], -2)) #  'c'
+print(nth(['a', 'b', 'c', 'd'], 0))  # 'a'
+print(nth(['a', 'b', 'c', 'd'], -4)) #  'a'
+print(nth(['a', 'b', 'c', 'd'], -1)) #  'd'
+print(nth(['a', 'b', 'c', 'd'], 3))  # 'd'
